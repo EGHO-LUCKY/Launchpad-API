39c2c410e50fca5cfb71dcbc84a26c896957303d
EGHO-LUCKY <egholucky8@gmail.com>
2026-03-03 13:42:36 +0100

    Add route to get personal ideas '/<user>/my-ideas'

--- route/userRoute.js
+++ route/userRoute.js
@@ -12,14 +12,15 @@ const router = express.Router();
 
 // ROUTES
 router.get("/", userController.home);
-router.post("/register", registrationValidator, registerLogin, userController.register);
-router.post("/login", authLogin, userController.login);
-router.get("/logout", logout, userController.logout);
-router.get("/:user/ideas", userAuth, userController.getIdeas);
-router.put("/:user/ideas", userAuth, ideaValidator, userController.putIdea);
-router.get("/:user/ideas/:ideaId", userAuth, userController.getIdea);
-router.patch("/:user/ideas/:ideaId", userAuth, userController.patchIdea);
-router.delete("/:user/ideas/:ideaId", userAuth, userController.deleteIdea);
-router.get("/:user/idea-category", userAuth, userController.ideaCategory);
+router.post("/register", registrationValidator, registerLogin, userController.register); // Registers User
+router.post("/login", authLogin, userController.login); // Logs in User
+router.get("/logout", logout, userController.logout); // Logs out User
+router.get("/:user/my-ideas", userAuth, userController.myIdeas); // Gets Ideas of logged in User only
+router.get("/:user/ideas", userAuth, userController.getIdeas); // Allows logged in User to Get all Ideas
+router.put("/:user/ideas/new", userAuth, ideaValidator, userController.putIdea); // Allows logged in User to Creates new Idea
+router.get("/:user/ideas/:ideaId", userAuth, userController.getIdea); // Allow logged in User to Get an Idea by its Id
+router.patch("/:user/ideas/:ideaId", userAuth, userController.patchIdea); // Allows logged in User to Updates Idea
+router.delete("/:user/ideas/:ideaId", userAuth, userController.deleteIdea); // Allows logged in User to Delete personal Idea 
+router.get("/:user/idea-category", userAuth, userController.ideaCategory); // Allows logged in User to get Ideas by idea category
 
 module.exports = router;
